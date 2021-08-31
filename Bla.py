6318d3f1dfa82f6a21c7bb7633d444e693314adb
davidinations <david19002@mail.unpad.ac.id>
2021-08-31 11:28:38 +0700

    Update Cuy

--- Bla.py
+++ Bla.py
@@ -1,4 +1,9 @@
 import numpy as np
 cvalues = [20.1, 20.8, 21.9, 22.5, 22.7, 22.3, 21.8, 21.2, 20.9, 20.1]
-C = np.sum(cvalues)
+C = np.array(cvalues)
 print(C)
+dvalues = [20.1, 20.8, 21.9, 22.5, 22.7, 22.3, 21.8, 21.2, 20.9, 20.1]
+D = np.array(dvalues)
+print(D)
+E = C * D
+print(E)
